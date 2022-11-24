52cd3983e0b24923d097e70c626d7cc245287329
Lisa-Tera <56185149+Lisa-Tera@users.noreply.github.com>
2022-11-24 15:08:06 +0900

    Update and rename 10_20365.py to 10_20365_블로그 참고.py

--- greedy_1124/10_20365_블로그 참고.py
+++ greedy_1124/10_20365_블로그 참고.py
@@ -13,5 +13,5 @@ for i in range(1, n):
     if RB[i] != RB[i-1]:#색 다르면 해당 색의 칠하는 횟수 +1
         colors[RB[i]]+=1
 
-result = min(colors['B'], colors['R'])+1 #칠할 횟수가 더 많은 것을 먼저 전체 칠하고(1) 칠할 횟수가 더 적은 것의 횟수 값(min)을 더한다.
+result = min(colors['B'], colors['R'])+1 #'B' :5   'R' :4 이므로 처음 색칠하는 색을 B로 잡아 남은 R값 더함
 print(result)
