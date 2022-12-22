5c7588d6a0ce63050fdd14886e10adc961c54bff
Lisa-Tera <eun01632@naver.com>
2022-12-22 15:35:07 +0900

    Update 2_20922.py

--- 1222_dyna+twopoint/2_20922.py
+++ 1222_dyna+twopoint/2_20922.py
@@ -2,19 +2,21 @@
 '''
 https://www.acmicpc.net/problem/20922 겹치는 건 싫어
 '''
-N,K = map(int, input().split())
+N, K = map(int, input().split())
 num = list(map(int, input().split()))
-cnt = []        
 
-    #start와 end사이에 겹치는 수가 K개 이하일 때는 end를 늘리며
-    # 최대 길이를 늘리고
-    # K개 초과일 때는 begin를 늘리며 겹치는 수가 빠질때까지 begin을 증가
-ans = 0
-end =1
-for be in range(N) :
-    while(end <=N and cnt[num[end]] < K):
-        cnt[num[end]]+=1
-        end +=1
-    ans = max(end-be,ans)    
-    cnt[num[be]] -=1
-print(ans)
+sequence = [0] * (max(num) + 1)
+left = 0
+right = 0
+result = 0
+
+while right < N:
+    if sequence[num[right]] != K:
+        sequence[num[right]] += 1
+        right += 1
+    else:
+        sequence[num[left]] -= 1
+        left += 1
+    
+    result = max(result, right-left)
+print(result)
